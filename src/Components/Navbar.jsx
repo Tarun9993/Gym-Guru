f089ff5b9b3d6c8c77e6d36de1081582b4cdba0e
Tarun <tarunkumartadala@gmail.com>
2025-01-08 11:11:27 +0530

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -162,7 +162,7 @@ function Navbar({ menuOpen, setMenuOpen }) {
           </button>
           <RxCross2
             size={28}
-            className="absolute top-5 right-0 m-12 text-white cursor-pointer"
+            className="absolute top-5 right-0 m-8 text-white cursor-pointer"
             onClick={handleCross}
           />
         </nav>
